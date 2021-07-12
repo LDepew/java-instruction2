ce0ac08b820d1b61a764fda6c1d1bd8b331290bc
Lance <Metal_Death_Music@yahoo.com>
2021-07-12 17:05:27 -0400

    Switch Statement

---
+++ JavaCourse/src/SwitchStatement.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SwitchStatement {
+	
+	public static void main(String [] args) {
+		
+		int age;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Please enter age:");
+		
+		age = sc.nextInt();
+		
+		switch(age) {
+		case 18:
+			System.out.println("Age 18");
+			break;
+		case 19:
+			System.out.println("Age 19");
+			break;
+		case 20:
+			System.out.println("Age 20");
+			break;
+		case 21:
+			System.out.println("Age 21");
+			break;
+		case 22:
+			System.out.println("Age 22");
+			break;
+			default:
+				System.out.println("Not Matched");
+				break;
+		}
+	}
+
+}
