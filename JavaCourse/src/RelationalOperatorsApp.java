26498fdc64463c8da660ef084c18a9c475d57423
Lance <Metal_Death_Music@yahoo.com>
2021-07-12 14:40:55 -0400

    Relational Operators

---
+++ JavaCourse/src/RelationalOperatorsApp.java
@@ -0,0 +1,52 @@
+
+public class RelationalOperatorsApp {
+	
+	public static void main(String [] args) {
+		
+		int num1 = 10;
+		int num2 = 50;
+		
+		if(num1 == num2) {
+			System.out.println("num1 and num2 are equal.");
+		}
+		else {
+			System.out.println("num1 and num2 are not equal.");
+		}
+		
+		if(num1 != num2) {
+			System.out.println("num1 and num2 are not equal.");
+		}
+		else {
+			System.out.println("num1 and num2 are equal.");
+		}
+		
+		if(num1 > num2) {
+			System.out.println("num1 greater than num2.");
+		}
+		else {
+			System.out.println("num1 not greater than num2.");
+		}
+		
+		if(num1 < num2) {
+			System.out.println("num1 less than num2.");
+		}
+		else {
+			System.out.println("num1 not less than num2.");
+		}
+		
+		if(num1 >= num2) {
+			System.out.println("num1 greater than or equal to num2.");
+		}
+		else {
+			System.out.println("num1 not greater than or equal to num2.");
+		}
+		
+		if(num1 <= num2) {
+			System.out.println("num1 less than or equal to num2.");
+		}
+		else {
+			System.out.println("num1 not less than or equal to num2.");
+		}
+	}
+
+}
