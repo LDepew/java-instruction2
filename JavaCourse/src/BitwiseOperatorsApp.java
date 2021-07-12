b85c71ccc856a0326d4c89ce5c37b99b73f2ef7d
Lance <Metal_Death_Music@yahoo.com>
2021-07-12 14:54:26 -0400

    Bitwise Operators

---
+++ JavaCourse/src/BitwiseOperatorsApp.java
@@ -0,0 +1,40 @@
+
+public class BitwiseOperatorsApp {
+	
+	public static void main(String [] args) {
+		
+		int num1 = 11;
+		int num2 = 22;
+		
+		int result = 0;
+		
+		result = num1 & num2;
+		
+		System.out.println("num1 & num2 " + result);
+		
+		result = num1 | num2;
+		
+		System.out.println("num1 | num2 " + result);
+		
+		result = num1 ^ num2;
+		
+		System.out.println("num1 ^ num2 " + result);
+		
+		result = num1;
+		
+		System.out.println("num1 ~ num2 " + result);
+		
+		result = num1 << 2;
+		
+		System.out.println("num1 << num2 " + result);
+		
+		result = num1 >> 2;
+		
+		System.out.println("num1 >> num2 " + result);
+		
+		result = num1 >>> 2;
+		
+		System.out.println("num1 >>> num2 " + result);
+	}
+
+}
