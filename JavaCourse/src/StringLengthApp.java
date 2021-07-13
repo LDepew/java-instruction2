b9613b138d823b0bf4000a79126df473175678de
Lance <Metal_Death_Music@yahoo.com>
2021-07-13 14:22:14 -0400

    String Length

---
+++ JavaCourse/src/StringLengthApp.java
@@ -0,0 +1,12 @@
+
+public class StringLengthApp {
+	
+	public static void main(String [] args) {
+		
+		String palindrome = "dot saw i was Tod";
+		int len = palindrome.length();
+		
+		System.out.println("String length is: " + len);
+	}
+
+}
