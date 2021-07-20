4173e3f1bc363941354c1df822945236899a9296
Lance <Metal_Death_Music@yahoo.com>
2021-07-20 15:21:47 -0400

    Single Diman Array

--- JavaCourse/src/SingleDimanArrayApp.java
+++ JavaCourse/src/SingleDimanArrayApp.java
@@ -14,7 +14,24 @@ public class SingleDimanArrayApp {
 		for(int i=0; i<a.length; i++) { 
 			System.out.println(a[i]);
 	}
+		
+		System.out.println();
+		
+		int iarr[] = new int[3];
+		
+		iarr[0] = 1;
+		iarr[1] = 2;
+		iarr[2] = 3;
+		
+		System.out.println(iarr[0]);
+		System.out.println(iarr[1]);
+		System.out.println(iarr[2]);
+		System.out.println();
 
+		for (int i = 0; i<iarr.length; i=i+1) {
+			System.out.println(iarr[i]);
+			System.out.println("");
+		}
 	}
 
 }
