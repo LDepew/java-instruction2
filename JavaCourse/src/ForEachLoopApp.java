8f75094661d511dd109d3d38aa5abc8d64c86491
Lance <Metal_Death_Music@yahoo.com>
2021-07-20 14:00:53 -0400

    For Each Loop:

---
+++ JavaCourse/src/ForEachLoopApp.java
@@ -0,0 +1,23 @@
+
+public class ForEachLoopApp {
+	
+	public static void main(String [] args) {
+		
+		int[] marks = {120, 130, 140, 150, 95, 100};
+		int highest_marks = maximum(marks);
+		
+		System.out.println("The highest score is " + highest_marks);
+	}
+
+
+
+public static int maximum(int[]numbers) {
+	int maxSoFar = numbers[0];
+	for (int num:numbers) {
+		if (num > maxSoFar) {
+			maxSoFar = num;
+		}
+	}
+	return maxSoFar;
+}
+}
