387874d73df2e2abec7cede140fe16b4325c7952
Lance <Metal_Death_Music@yahoo.com>
2021-07-26 17:43:53 -0400

    Date Time

---
+++ JavaCourse/src/DateTimeApp.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class DateTimeApp {
+	
+	public static void main(String [] args) {
+		
+		Date date = new Date();
+		
+		System.out.println(date.toString());
+		
+	}
+
+}
