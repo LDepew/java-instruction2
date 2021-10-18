0e1f63a138af225a511b06d948f10138faa0869b
Lance <Metal_Death_Music@yahoo.com>
2021-10-18 17:48:54 -0400

    Scanner

--- Help/src/Main.java
+++ Help/src/Main.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		int x = 123;
-		double y = 3.14;
-		boolean z = false;
-		char symbol = '@';
-		String name = "Bro";
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("What is your name?");
+		String name = scanner.nextLine();
+		System.out.println("How old are you?");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("What is your favorite food?");
+		String food = scanner.nextLine();
 		
 		System.out.println("Hello " + name);
+		System.out.println("You are " + age + " years old");
+		System.out.println("You like " + food);
 		
 
 	}
