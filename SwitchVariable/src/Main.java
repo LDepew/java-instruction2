b405ba847568defaa0ccfc32c4f85e9e6dcfcbc8
Lance <Metal_Death_Music@yahoo.com>
2021-10-18 17:24:40 -0400

    Switch Variables

---
+++ SwitchVariable/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String x = "Water";
+		String y = "Kool-Aid";
+		String temp;
+		
+		temp = x;
+		x = y;
+		y = temp;
+			
+		System.out.println("X: " + x);
+		System.out.println("Y: " + y);
+
+	}
+
+}
