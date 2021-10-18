2ecb2bf952862a2f1ecc8c5b5bd60ec1fffa0b7b
Lance <Metal_Death_Music@yahoo.com>
2021-10-18 17:15:14 -0400

    Help

---
+++ Help/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 123;
+		double y = 3.14;
+		boolean z = false;
+		char symbol = '@';
+		String name = "Bro";
+		
+		
+		System.out.println("Hello " + name);
+		
+
+	}
+
+}
