f50534808ba8ceef044fd698ca26798071596eac
Lance <Metal_Death_Music@yahoo.com>
2021-10-19 14:38:17 -0400

    expressions

--- Help/src/Main.java
+++ Help/src/Main.java
@@ -1,25 +1,15 @@
-import java.util.Scanner;
-
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+	 double friends = 10;
 		
-		System.out.println("What is your name?");
-		String name = scanner.nextLine();
-		System.out.println("How old are you?");
-		int age = scanner.nextInt();
-		scanner.nextLine();
-		System.out.println("What is your favorite food?");
-		String food = scanner.nextLine();
+		friends = friends / 3;
 		
-		System.out.println("Hello " + name);
-		System.out.println("You are " + age + " years old");
-		System.out.println("You like " + food);
+		System.out.println(friends);
 		
-
+				
 	}
 
 }
