338162f9bb4e1cc8dac08e0ff96c03d2f22833d0
Lance <Metal_Death_Music@yahoo.com>
2021-11-17 03:01:35 -0500

    GTO

---
+++ GTO/src/GTO.java
@@ -0,0 +1,15 @@
+
+public class GTO {
+	
+	public static void main (String[]args) {
+		
+		System.out.println("Subhash");
+		System.out.println("5 * 4 / 4 - 3.5");
+		System.out.println( 5 * 4 / 4 - 3.5 );
+		System.out.println( 5 + "Subhash" );
+		System.out.println( "Subhash " + 5);
+		System.out.println( 5 + 5 + "Subhash" + 5);
+		System.out.println( 5 + "Subhash" + 5 + "Subhash");
+		System.out.println( "Subhash" + 5 + 5);
+	}
+}
