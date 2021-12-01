9f611926f4c7913be5bb6edac180518ac6a80f72
Lance <Metal_Death_Music@yahoo.com>
2021-12-01 17:51:12 -0500

    GetBits Update

--- GetBits/src/GetBits.java
+++ GetBits/src/GetBits.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
 
 public class GetBits {
 	
 	public static void main (String [] args) {
 		
-		int x = 182;
-		int p = 4;
-		int n = 3;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter x value");
+		int x = sc.nextInt();
+		System.out.println("Enter p value");
+		int p = sc.nextInt();
+		System.out.println("Enter n value");
+		int n = sc.nextInt();
 		
 		int x2 = x >> (p + 1 - n) & ~(~0 << n);
 		
 		System.out.println("After getbits x = " + x2);
+		
 	}
 
 }
