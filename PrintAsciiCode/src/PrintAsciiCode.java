2dbacb579845c7b2dbbf6f790d971e932209127e
Lance <Metal_Death_Music@yahoo.com>
2021-12-06 13:38:02 -0500

    Print Ascii Code

---
+++ PrintAsciiCode/src/PrintAsciiCode.java
@@ -0,0 +1,25 @@
+
+public class PrintAsciiCode {
+	
+	public static void main(String [] args) {
+		
+		System.out.println(" Ascii for 2 = " + (int)'2');
+		System.out.println(" Ascii for 3 = " + (int)'3');
+		System.out.println(" Ascii for a = " + (int)'a');
+		System.out.println(" Ascii for Z = " + (int)'Z');
+		System.out.println();
+		System.out.println(" Character for 50 = " + (char)50);
+		System.out.println(" Character for 65 = " + (char)65);
+		System.out.println(" Character for 66 = " + (char)66);
+		System.out.println(" Character for 99 = " + (char)99);
+		System.out.println(" Character for 107 = " + (char)107);
+		System.out.println();
+		System.out.println(" Character for 4B = " + (char)0x4B);
+		System.out.println(" Character for 54 = " + (char)0x54);
+		System.out.println(" Character for 57 = " + (char)0x57);
+		System.out.println(" Character for AD = " + (char)0xAD);
+		System.out.println(" Character for 6C = " + (char)0x6C);
+		
+	}
+
+}
