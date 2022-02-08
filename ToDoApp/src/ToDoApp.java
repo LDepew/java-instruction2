fec99d342ea45d46e6884b7021af1702ad056aef
Lance <Metal_Death_Music@yahoo.com>
2022-02-08 15:52:44 -0500

    test

---
+++ ToDoApp/src/ToDoApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ToDoApp {
+	
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter something to do");
+		String Thing1 = sc.nextLine();
+		
+		System.out.println(Thing1);
+	}
+
+}
